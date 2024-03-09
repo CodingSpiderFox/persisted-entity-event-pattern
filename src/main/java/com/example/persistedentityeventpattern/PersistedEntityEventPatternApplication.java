9e40f6e3a1de8f5b0899c4805d80a44f14d48031
codingspiderfox <codingspiderfox@gmail.com>
2024-03-09 21:13:49 +0100

    feat: add  spring

---
+++ src/main/java/com/example/persistedentityeventpattern/PersistedEntityEventPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.persistedentityeventpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistedEntityEventPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersistedEntityEventPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/persistedentityeventpattern/PersistedEntityEventPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.persistedentityeventpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistedEntityEventPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
